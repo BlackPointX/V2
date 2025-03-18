12c62f24d02335f0090325cdc726b8267b1ff206
BlackPointX <mateuszczarnecki90@gmail.com>
2025-03-18 14:38:11 +0100

    Update form.js

--- form.js
+++ form.js
@@ -124,7 +124,7 @@ document.addEventListener('DOMContentLoaded', async () => {
             betLigaRow.classList.add('row');
 
             const betLigaLink = document.createElement('a');
-            betLigaLink.href = 'https://forms.gle/Cf7Z8jdnfUSZwDwE9';
+            betLigaLink.href = 'https://forms.gle/DfMoWrxHFdEswx396';
             betLigaLink.textContent = 'Formularz BetLiga';
             betLigaLink.classList.add('link');
             betLigaRow.appendChild(betLigaLink);
